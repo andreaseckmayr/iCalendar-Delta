2247b078ff43e1a246a8824f586a33829fe4d652
Andreas Eckmayr <andreas.eckmayr@gmail.com>
2023-03-13 14:12:27 +0100

    rename class; consolidate more functions into wrapper class

--- ical_delta.py
+++ ical_delta.py
@@ -13,36 +13,38 @@ from icalendar import Calendar
 import urllib3
 
 # %%
-class Cal:
+class iCalWrapper:
+    '''class provides basic funtionality for loading and storing ical objects'''
     def __init__ (self, url: str, name: str, uid: str):
         self.url = url
         self.name = name
         self.uid = uid
-    
+        self.cal = None
+        self.uids = None
+
+    def load_from_url(self) -> Calendar:
+        '''load calendar from url'''
+        http = urllib3.PoolManager()
+        r = http.request('GET', self.url)
+        self.cal = Calendar.from_ical(r.data)
+        r.close()
+        return self.cal
+
+    def get_uids(self) -> Set[str]:
+        '''get uids from calendar entries as set'''
+        uids = []
+        for comp in self.cal.walk():
+            if comp.name == 'VEVENT':
+                uids.append(comp.get('uid'))
+                #print(comp.get('summary'))
+        self.uids = set(uids)
+        return self.uids
+
     def __str__(self):
         return f'{self.url}'
 
 # %%
-def get_cal(url: str) -> Calendar:
-    '''load calendar from url'''
-    http = urllib3.PoolManager()
-    r = http.request('GET', url)
-    cal = Calendar.from_ical(r.data)
-    r.close()
-    return cal
-
-# %%
-def get_uids(cal: Calendar) -> Set[str]:
-    '''get uids from calendar entries as set'''
-    uids = []
-    for comp in cal.walk():
-        if comp.name == 'VEVENT':
-            uids.append(comp.get('uid'))
-            #print(comp.get('summary'))
-    return set(uids)
-
-# %%
-def compare(a: Cal, b: Cal) -> Calendar:
+def compare(a: iCalWrapper, b: iCalWrapper) -> Calendar:
     '''take two calendars and return a new cal with missing entries'''
     cal = Calendar()
     cal.add('prodid', f"{a.name}//ndrs.dev")
@@ -58,26 +60,25 @@ calendars = dict()
 with open('/config/calendars.csv', 'r', encoding='UTF-8') as file:
     csv_reader = csv.reader(file, delimiter=';')
     for row in csv_reader:
-        calendars[row[0]] = Cal(row[1], row[2], row[3])
+        calendars[row[0]] = iCalWrapper(row[1], row[2], row[3])
 
 # %%  load calendars
 for _, item in calendars.items():
-    c = get_cal(item.url)
-    item.cal = c
-    item.uids = get_uids(c)
+    item.load_from_url()
+    item.get_uids()
 
 # %%  compare calendars and add missing events
 cal_a_delta = compare(calendars['1'], calendars['2'])
 cal_b_delta = compare(calendars['2'], calendars['1'])
 
 # %%  write calendars to file
-f = open(os.path.join('/calendars', f'{calendars["1"].hash}.ics'), 'wb')  # $ echo -n andreas | md5sum
+f = open(os.path.join('/calendars', f'{calendars["1"].uid}.ics'), 'wb')  # $ echo -n andreas | md5sum
 f.write(cal_a_delta.to_ical())
 f.close()
 
-f = open(os.path.join('/calendars', f'{calendars["2"].hash}.ics'), 'wb')
+f = open(os.path.join('/calendars', f'{calendars["2"].uid}.ics'), 'wb')
 f.write(cal_b_delta.to_ical())
 f.close()
 
 # %%
-print(f'{datetime.datetime.now()}: cal sync completed')
+print(f'{datetime.now()}: cal sync completed')
